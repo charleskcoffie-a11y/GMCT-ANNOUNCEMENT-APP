c9ec25826d5474aa7e75f07b185c3c0ed86d0dfa
charleskcoffie-a11y <charleskcoffie@gmail.com>
2026-03-10 16:37:37 -0400

    Enable Firebase cloud sync with project config

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -6,15 +6,16 @@
   3) Keep the same room name on all devices that should share data.
 */
 
-window.GMCT_FIREBASE_ENABLED = false;
+window.GMCT_FIREBASE_ENABLED = true;
 window.GMCT_FIREBASE_ROOM = 'gmct-main';
 window.GMCT_FIREBASE_USE_ANON_AUTH = true;
 
 window.GMCT_FIREBASE_CONFIG = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  appId: '',
-  messagingSenderId: '',
-  storageBucket: ''
+  apiKey: 'AIzaSyATzWRb3PwmwUGYMJ-cyfd5I6D5RC1DD0c',
+  authDomain: 'gmct-announcement.firebaseapp.com',
+  projectId: 'gmct-announcement',
+  storageBucket: 'gmct-announcement.firebasestorage.app',
+  messagingSenderId: '112212748992',
+  appId: '1:112212748992:web:1b1b70df7d023a71da9b25',
+  measurementId: 'G-B9R9M5JQZF'
 };
